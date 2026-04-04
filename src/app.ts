957e5a19c3e5e54a3adf4be241219f2ded86c633
Ahmed El Kashif <ahmedmostafaworkemail@gmail.com>
2026-04-05 01:42:32 +0200

    added .env.example so other devs remember to implement the .env when they pull the repo

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,11 @@ import cors from 'cors'
 import helmet from 'helmet'
 import morgan from 'morgan'
 import { AppError } from './core/errors/AppError'
-import { globalErrorHandler } from './core/middlewares/error.middleware' 
-import path from 'path'; 
-
+import { globalErrorHandler } from './core/middlewares/error.middleware'
+import path from 'path'
 import authRoutes from './features/auth/auth.routes'
-import userRoutes from './features/users/user.routes';
+import userRoutes from './features/users/user.routes'
+
 const app: Application = express()
 
 // Global Middlewares
@@ -15,19 +15,18 @@ app.use(helmet()) // Security headers
 app.use(cors()) // Allow frontend to communicate
 app.use(express.json()) // Parse incoming JSON payloads
 app.use(morgan('dev')) // Log HTTP requests in the terminal
- 
-// Serve the uploads folder as static files
-app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
 
+// Serve the uploads folder as static files
+app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')))
 
 // Health Check Route (Good for ensuring the API is awake)
 app.get('/api/health', (req: Request, res: Response) => {
   res.status(200).json({ success: true, message: 'Context API is running smoothly.' })
 })
 
-app.use('/api/auth', authRoutes) 
+app.use('/api/auth', authRoutes)
 
-app.use('/api/users', userRoutes);
+app.use('/api/users', userRoutes)
 // 404 Handler for undefined routes
 app.use((req: Request, res: Response, next: NextFunction) => {
   next(new AppError(`Route ${req.originalUrl} not found`, 404))
